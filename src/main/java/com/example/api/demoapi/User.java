799d16e79cb07574cb9b684a6f33214702c654f6
Casey_Ryback <serega126@list.ru>
2024-01-31 15:45:29 +0300

    Users data base handler added

--- src/main/java/com/example/api/demoapi/User.java
+++ src/main/java/com/example/api/demoapi/User.java
@@ -1,8 +1,17 @@
 package com.example.api.demoapi;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name", nullable = false, length = 50)
     private String name;
+    @Column(name = "age", nullable = false)
     private int age;
+    @Column(name = "email", unique = true, nullable = false, length = 50)
     private String email;
 
     public String getName() {
@@ -16,6 +25,11 @@ public class User {
     public int getAge() {
         return age;
     }
+
+    public Long getId() {
+        return id;
+    }
+
 
     public void setAge(int age) {
         this.age = age;
--- src/main/java/com/example/api/demoapi/UserController.java
+++ src/main/java/com/example/api/demoapi/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,17 +13,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    private final UserRepository userRepository;
     private final UserService userService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserRepository userRepository, UserService userService) {
+        this.userRepository = userRepository;
         this.userService = userService;
     }
+
 
     @RequestMapping(
             method = RequestMethod.POST)
     public ResponseEntity<User> createUser(@RequestParam String name, @RequestParam int age, @RequestParam String email) {
         return new ResponseEntity<>(userService.createUser(name, age, email), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "users";
     }
 
 }
